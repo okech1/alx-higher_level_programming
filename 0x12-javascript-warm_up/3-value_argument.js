1a6906d184eb7ad9bb94a24e4161e76f5af3b143
Livingstone Okech <oklivinstone@gmail.com>
2024-04-22 13:31:19 -0700

    updates

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
+// prints the first argument passed to it
 
-// Print the first argument passed to the script
-const arg = process.argv[2];
-
-if (arg === undefined) {
-    console.log('No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(arg);
+  console.log(process.argv[2]);
 }
