a66a4e77b40f741cf798085b59c9a0fcc600a294
Livingstone Okech <oklivinstone@gmail.com>
2024-04-22 13:34:18 -0700

    changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+// Prints a message depending of the number of arguments passed
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-    console.log('No argument');
-} else if (args.length === 1) {
-    console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
